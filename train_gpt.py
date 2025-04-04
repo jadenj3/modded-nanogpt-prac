03d5ed4041f9292af3f733a5020d52946585f900
Jaden Johnson <jadenjohnson@fb.com>
2025-04-04 17:31:14 -0400

    new initialization for skip weights

--- train_gpt.py
+++ train_gpt.py
@@ -365,8 +365,8 @@ class GPT(nn.Module):
         assert num_layers % 2 == 0
         self.skip_weights = nn.Parameter(torch.empty(num_layers // 2))
         fan_in = num_layers // 2
-        bound = 1 / math.sqrt(fan_in)
-        nn.init.normal_(self.skip_weights, -bound, bound)
+        std = 1 / math.sqrt(fan_in)  # Standard deviation
+        nn.init.normal_(self.skip_weights, mean=0.0, std=std)
         #self.residual_weights = nn.Parameter(torch.empty(num_layers, model_dim))
         # Apply Kaiming uniform initialization (what nn.Linear uses by default)
         #fan_in = num_layers//2  # Each row has num_layers inputs
