dff334657a118e3e544c6c31549e21362c29ae76
Jaden Johnson <jadenjohnson@fb.com>
2025-04-04 18:12:45 -0400

    back to messing around with denseformer

--- train_gpt.py
+++ train_gpt.py
@@ -367,9 +367,11 @@ class GPT(nn.Module):
         #fan_in = num_layers // 2
         #std = 1 / math.sqrt(fan_in)  # Standard deviation
         #nn.init.normal_(self.skip_weights, mean=0.0, std=std)
-        self.residual_weights = nn.Parameter(torch.empty(num_layers, model_dim))
-        # Apply Kaiming normal initialization
-        nn.init.kaiming_normal_(self.residual_weights, a=math.sqrt(5), mode='fan_in', nonlinearity='leaky_relu')
+        self.residual_weights = nn.Parameter(torch.ones(num_layers, model_dim))
+        # Apply Kaiming uniform initialization (what nn.Linear uses by default)
+        #fan_in = num_layers//2  # Each row has num_layers inputs
+        #bound = 1 / math.sqrt(fan_in)
+        #init.kaiming_uniform_(self.residual_weights, a=math.sqrt(5))
 
     def create_blockmasks(self, input_seq: Tensor, sliding_window_num_blocks: Tensor):
         BLOCK_SIZE = 128
