bb1760869b7c99bdb590becb8eb39c46fc739e98
Jaden Johnson <jadenjohnson@fb.com>
2025-04-04 20:59:41 -0400

    back to messing around with denseformer (LR adjustment)

--- train_gpt.py
+++ train_gpt.py
@@ -441,13 +441,13 @@ class GPT(nn.Module):
         }
 
         # Before the loop:
-        prev_x = None  # Start with the initial normalized embeddings
+        prev_x = []  # Start with the initial normalized embeddings
         for i in range(len(self.blocks)):
             # Inside the loop for layer i:
             if prev_x:
-                x = x + self.residual_weights[i][0]*prev_x  # Get weights for layer i
+                x = x + self.residual_weights[i][0]*prev_x[0]  # Get weights for layer i
             x = self.blocks[i](x, ve[i], x0, block_masks[i])
-            prev_x = x
+            prev_x.append(x)
         '''
         for i in range(len(self.blocks)):
             if i in skip_map:
