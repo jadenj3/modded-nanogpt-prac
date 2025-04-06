cf4f319a77a651ead90227250f09a32ac741ce4e
Jaden Johnson <jadenjohnson@fb.com>
2025-04-05 22:48:48 -0400

    back to denseformer all prev layers 0.5 init

--- train_gpt.py
+++ train_gpt.py
@@ -366,7 +366,7 @@ class GPT(nn.Module):
         # Add learnable skip connection weights for decoder layers
         assert num_layers % 2 == 0
         #self.skip_weights = nn.Parameter(torch.ones(num_layers // 2))
-        self.residual_weights = nn.Parameter(torch.ones(num_layers, num_layers))
+        self.residual_weights = nn.Parameter(torch.full((num_layers, num_layers), 0.5))
         #fan_in = num_layers // 2
         #std = 1 / math.sqrt(fan_in)  # Standard deviation
         #nn.init.normal_(self.skip_weights, mean=0.0, std=std)
