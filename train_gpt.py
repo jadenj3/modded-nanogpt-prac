87c364fcc821afbd7edb8a55560638ec0e7ab988
Jaden Johnson <jadenjohnson@fb.com>
2025-04-05 23:35:22 -0400

    back to denseformer all prev feature weights layers 1 init bfloat 16(fix bug, should actually be all previous layers now)

--- train_gpt.py
+++ train_gpt.py
@@ -367,8 +367,8 @@ class GPT(nn.Module):
         assert num_layers % 2 == 0
         #self.skip_weights = nn.Parameter(torch.ones(num_layers // 2))
         #self.residual_weights = nn.Parameter(torch.ones(num_layers, num_layers))
-        self.residual_weights = nn.Parameter(torch.full((num_layers, num_layers), 1.0, dtype=torch.bfloat16))
-
+        #self.residual_weights = nn.Parameter(torch.full((num_layers, num_layers), 1.0, dtype=torch.bfloat16))
+        self.residual_weights = nn.Parameter(torch.ones(num_layers, num_layers, model_dim, dtype=torch.bfloat16))
         #fan_in = num_layers // 2
         #std = 1 / math.sqrt(fan_in)  # Standard deviation
         #nn.init.normal_(self.skip_weights, mean=0.0, std=std)
