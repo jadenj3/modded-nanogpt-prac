a0004862ad3583992f181a5725e7b4ab3fae228c
Jaden Johnson <jadenjohnson@fb.com>
2025-04-06 14:24:35 -0400

    SOTA prev 1 denseformer, beats skips at all LR, clean up code

--- train_gpt.py
+++ train_gpt.py
@@ -444,8 +444,7 @@ class GPT(nn.Module):
         prev_layers = []
         for i in range(len(self.blocks)):
             # Inside the loop for layer i:
-            for j in range(len(prev_layers)):
-                x = self.residual_weights[i][j] * prev_layers[j]  # Get weights for layer i
+            x = self.residual_weights[i][i] * prev_layers[0]  # Get weights for layer i
             x = self.blocks[i](x, ve[i], x0, block_masks[i])
             if prev_layers:
                 prev_layers.pop()
