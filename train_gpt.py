c315fe0b86b0f1c5c23f843db409077cd70f6cb7
Jaden Johnson <jadenjohnson@fb.com>
2025-04-06 15:31:33 -0400

    skip weights seed 5 (prev was SOTA prev 1)

--- train_gpt.py
+++ train_gpt.py
@@ -442,13 +442,13 @@ class GPT(nn.Module):
             11: 2,
         }
         prev_layers = []
-
+        '''
         for i in range(len(self.blocks)):
             # Inside the loop for layer i:
             for j in range(len(prev_layers)):
                 x = self.residual_weights[i][j]*prev_layers[j]  # Get weights for layer i
             x = self.blocks[i](x, ve[i], x0, block_masks[i])
-            prev_layers.append(x)
+            prev_layers.append(x)'''
 
         for i in range(len(self.blocks)):
             if i in skip_map:
