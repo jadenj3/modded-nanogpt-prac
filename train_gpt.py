0b37ecf99c275217c329f7bc37a33ba42202e50c
Jaden Johnson <jadenjohnson@fb.com>
2025-04-09 17:13:18 -0400

    back to skip weights, propogate skip values to block, norm v

--- train_gpt.py
+++ train_gpt.py
@@ -520,7 +520,7 @@ class Hyperparameters:
     train_files = "data/fineweb10B/fineweb_train_*.bin" # input .bin to train on
     val_files = "data/fineweb10B/fineweb_val_*.bin" # input .bin to eval validation loss on
     val_tokens = 10485760 # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
-    train_seq_len = 78*1024 # FlexAttention sequence length
+    train_seq_len = 128*1024 # FlexAttention sequence length
     val_seq_len = 4*64*1024 # FlexAttention sequence length for validation
     # optimization
     num_iterations = 405 # number of iterations to run
