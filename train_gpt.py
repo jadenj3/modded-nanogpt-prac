dba53bdaed52debd2daf60875f1474fe65960cd7
Jaden Johnson <jadenjohnson@fb.com>
2025-04-09 14:45:02 -0400

    back to skip weights, propogate skip values to block, norm v

--- train_gpt.py
+++ train_gpt.py
@@ -286,7 +286,7 @@ class CausalSelfAttention(nn.Module):
         # scale the attention logits by given constant, instead of the default head_dim**-0.5, by @leloykun
         # inspired by learnable scalars used by @brendanh0gan https://x.com/hi_tysam/status/1879693583898591283
         self.attn_scale = 0.12
-        self.skip_lambdas = nn.Parameter(torch.tensor([1.0, 1.0]))
+        #self.skip_lambdas = nn.Parameter(torch.tensor([1.0, 1.0]))
 
     def forward(self, x: Tensor, ve: Tensor | None, block_mask: BlockMask, skip_values):
         B, T = x.size(0), x.size(1) # batch size, sequence length
@@ -295,8 +295,8 @@ class CausalSelfAttention(nn.Module):
         q, k = norm(q), norm(k) # QK norm @Grad62304977
         q, k = self.rotary(q), self.rotary(k)
         v = norm(v)
-        if skip_values is not None:
-                v = self.skip_lambdas[0] * v + self.skip_lambdas[1] * skip_values.view_as(v)
+        #if skip_values is not None:
+                #v = self.skip_lambdas[0] * v + self.skip_lambdas[1] * skip_values.view_as(v)
         if ve is not None:
             v = self.lambdas[0] * v + self.lambdas[1] * ve.view_as(v) # @KoszarskyB & @Grad62304977
         else: # skip mid-layers token value embeddings by @YouJiacheng
