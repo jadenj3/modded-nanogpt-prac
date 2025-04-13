42a9b0cf27a772eb894ea9fc6439ebb0b0a56f45
Jaden Johnson <jadenjohnson@fb.com>
2025-04-13 00:04:01 -0400

    feature weights seed 999 1215 iterations normal skip weights, gated x0 and ve

--- train_gpt.py
+++ train_gpt.py
@@ -486,7 +486,7 @@ class Hyperparameters:
     train_seq_len = 64*1024 # FlexAttention sequence length
     val_seq_len = 4*64*1024 # FlexAttention sequence length for validation
     # optimization
-    num_iterations = 1200 # number of iterations to run
+    num_iterations = 1215 # number of iterations to run
     cooldown_frac = 0.6 # fraction of training spent cooling down the learning rate
     # architecture
     vocab_size = 50257
