abcd8d5f67946e32549029ce3bf569fab379cc96
Jaden Johnson <jadenjohnson@fb.com>
2025-04-14 20:49:53 -0400

    Trying new block size stuff with new lr

--- train_gpt.py
+++ train_gpt.py
@@ -557,22 +557,23 @@ for opt in optimizers:
         group["initial_lr"] = group["lr"]
 import math
 # learning rate schedule: stable then decay
-'''
+
 def get_lr(step: int):
     # Apply modulo to make step cycle from 0 to 1199
     x = step / args.num_iterations  # progress in training
     assert 0 <= x < 1
 
     # Apply the new formula: 1 - 0.95x²
-    return 1.0 - 0.95 * (x ** 2)'''
+    return 1.0 - 0.95 * (x ** 2)
 
+'''
 def get_lr(step: int):
     x = step / args.num_iterations # progress in training
     assert 0 <= x < 1
     if x < 1 - args.cooldown_frac:
         return 1.0
     else:
-        return (1 - x) / args.cooldown_frac
+        return (1 - x) / args.cooldown_frac'''
 
 # attention window size schedule: linearly increase
 @lru_cache(1)
