a953f603c6bce731c991c2f91a4d53c626d89104
jadenj3 <jaden8000@gmail.com>
2025-06-19 18:00:18 -0400

    no world size assert

--- train_gpt.py
+++ train_gpt.py
@@ -361,7 +361,7 @@ run_id = int(os.environ.get("RUN_ID", 0))
 # torchrun sets these env variables
 rank = int(os.environ["RANK"])
 world_size = int(os.environ["WORLD_SIZE"])
-assert world_size == 8 # this code is designed for 8xH100
+#assert world_size == 8 # this code is designed for 8xH100
 assert torch.cuda.is_available()
 device = torch.device("cuda", int(os.environ["LOCAL_RANK"]))
 torch.cuda.set_device(device)
