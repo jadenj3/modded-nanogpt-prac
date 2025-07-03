cdfee9155b0dc070e9edef6ad92b03d7ebdfa476
Jaden Johnson <jadenjohnson@fb.com>
2025-07-02 22:16:59 -0400

    re-add feature weights

--- train_gpt.py
+++ train_gpt.py
@@ -242,7 +242,7 @@ class GPT(nn.Module):
         # Add learnable skip connection weights for decoder layers
         assert num_layers % 2 == 0
         self.skip_weights = nn.Parameter(torch.ones(num_layers, dtype=torch.bfloat16))
-        #self.feature_weights = nn.Parameter(torch.ones(model_dim, dtype=torch.bfloat16))
+        self.feature_weights = nn.Parameter(torch.ones(model_dim, dtype=torch.bfloat16))
 
     def create_blockmasks(self, input_seq: Tensor, sliding_window_num_blocks: Tensor):
         BLOCK_SIZE = 128
@@ -306,7 +306,7 @@ class GPT(nn.Module):
         }
         for i in range(len(self.blocks)):
             if i in skip_map:
-                x = x + self.skip_weights[skip_map[i]] *skip_connections[skip_map[i]]
+                x = x + self.skip_weights[skip_map[i]] *self.feature_weights * skip_connections[skip_map[i]]
             x = self.blocks[i](x, ve[i], x0, block_masks[i])
             skip_connections.append(x)
 
