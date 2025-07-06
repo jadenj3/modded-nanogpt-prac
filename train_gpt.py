1fdd4e750591b86a2cb78cc864d418b608516833
Jaden Johnson <jadenjohnson@fb.com>
2025-07-06 04:55:15 -0400

    shorterer blockmask seed 5 add new mlps

--- train_gpt.py
+++ train_gpt.py
@@ -206,6 +206,7 @@ class Block(nn.Module):
         # skip attention of blocks.7 (the 8th layer) by @YouJiacheng
         self.attn = CausalSelfAttention(dim, num_heads, max_seq_len) if layer_idx != 7 else None
         self.mlp = MLP(dim)
+        self.mlp2 = MLP(dim)
         self.lambdas = nn.Parameter(torch.tensor([1.0, 0.0]))
         self.record = nn.Buffer(torch.tensor([0.0, 0.0, 0.0]))
 
@@ -218,7 +219,8 @@ class Block(nn.Module):
             if not self.training:
                 self.record[1].lerp_(torch.square(z).mean(dtype=torch.float32), 0.5)
             x = x + z
-        z = self.mlp(norm(x))
+        x = norm(x)
+        z = self.mlp(x) + self.mlp2(x)
         if not self.training:
             self.record[2].lerp_(torch.square(z).mean(dtype=torch.float32), 0.5)
         x = x + z
