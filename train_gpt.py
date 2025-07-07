deb43cf4855a698c0c0a2c0059e1960cf0435daa
Jaden Johnson <jadenjohnson@fb.com>
2025-07-07 01:30:05 -0400

    shorterer blockmask seed 5 rotary every other layer

--- train_gpt.py
+++ train_gpt.py
@@ -175,7 +175,7 @@ class CausalSelfAttention(nn.Module):
         assert B == 1, "Must use batch size = 1 for FlexAttention"
         q, k, v = F.linear(x, self.qkvo_w[:3].flatten(end_dim=1)).view(B, T, 3 * self.num_heads, self.head_dim).chunk(3, dim=-2)
         q, k = norm(q), norm(k) # QK norm @Grad62304977
-        if self.layer_idx % 2 == 0: #change to !=
+        if self.layer_idx % 2 != 0: #change to !=
             q, k = self.rotary(q), self.rotary(k)
         v = norm(v)
         if ve is not None:
