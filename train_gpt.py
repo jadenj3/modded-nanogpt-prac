10c23d100951dca84f6ad2de5c3cdcf0fc714dd4
Jaden Johnson <jadenjohnson@fb.com>
2025-07-08 02:22:49 -0400

    shorterer blockmask tryin some weird stuff

--- train_gpt.py
+++ train_gpt.py
@@ -183,6 +183,7 @@ class CausalSelfAttention(nn.Module):
         q, k, v = F.linear(x, self.qkvo_w[:3].flatten(end_dim=1)).view(B, T, 3 * self.num_heads, self.head_dim).chunk(3, dim=-2)
         q, k = norm(q), norm(k) # QK norm @Grad62304977
         q, k = self.rotary(q), self.rotary(k)
+        q,k = quasicrystal_transform(q), quasicrystal_transform(k)
         v = norm(v)
         if ve is not None:
             v = self.lambdas[0] * v + self.lambdas[1] * ve.view_as(v) # @KoszarskyB & @Grad62304977
@@ -229,7 +230,7 @@ class Block(nn.Module):
         z = self.mlp(norm(x))
         if not self.training:
             self.record[2].lerp_(torch.square(z).mean(dtype=torch.float32), 0.5)
-        x = x + quasicrystal_transform(z)
+        x = x + z
         return x
 
 # -----------------------------------------------------------------------------
