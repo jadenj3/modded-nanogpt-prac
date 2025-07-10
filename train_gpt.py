8382240e0a7c5240d7571e1a1763b6a82265e8d8
Jaden Johnson <jadenjohnson@fb.com>
2025-07-10 18:46:19 -0400

    adding new blockmasks, get rid of inductor stuff.

--- train_gpt.py
+++ train_gpt.py
@@ -281,7 +281,7 @@ class GPT(nn.Module):
                 mask_mod=document_causal,
             )
         # Long-short SWA block masks by @leloykun & @YouJiacheng, adapated from suggestion by @Grad62304977, following Gemma 2 paper
-        return build_bm(sliding_window_num_blocks), build_bm(sliding_window_num_blocks // 6), build_bm(sliding_window_num_blocks // 2)
+        return build_bm(sliding_window_num_blocks), build_bm(sliding_window_num_blocks // 6), build_bm(sliding_window_num_blocks)
 
     def forward(self, input_seq: Tensor, target_seq: Tensor, sliding_window_num_blocks: Tensor):
         assert input_seq.ndim == 1
