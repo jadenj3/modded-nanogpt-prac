eed85108790d772f5736dcab6a9a601cee089fc7
Jaden Johnson <jadenjohnson@fb.com>
2025-09-24 17:07:19 -0400

    add prev_input as value residual,pre norm. fix dims

--- train_gpt.py
+++ train_gpt.py
@@ -171,10 +171,11 @@ class CausalSelfAttention(nn.Module):
         B, T = x.size(0), x.size(1) # batch size, sequence length
         assert B == 1, "Must use batch size = 1 for FlexAttention"
         q, k, v = F.linear(x, self.qkvo_w[:3].flatten(end_dim=1)).view(B, T, 3 * self.num_heads, self.head_dim).chunk(3, dim=-2)
-        carry = prev_input.to(device=x.device, dtype=v.dtype).view_as(q)
-        q = q + carry
+        carry = prev_input.to(device=x.device, dtype=v.dtype).view_as(v)
+        #q = q + carry
         q, k = norm(q), norm(k) # QK norm @Grad62304977
         q, k = self.rotary(q), self.rotary(k)
+        v = v + carry
         v = norm(v)
         if ve is not None:
             v = self.lambdas[0] * v + self.lambdas[1] * ve.view_as(v) # @KoszarskyB & @Grad62304977
