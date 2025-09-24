faa7bb3fc49a2cd886c16888cb315bb6f87299bb
Jaden Johnson <jadenjohnson@fb.com>
2025-09-24 16:59:25 -0400

    add prev_input as query residual, fix dims

--- train_gpt.py
+++ train_gpt.py
@@ -171,11 +171,11 @@ class CausalSelfAttention(nn.Module):
         B, T = x.size(0), x.size(1) # batch size, sequence length
         assert B == 1, "Must use batch size = 1 for FlexAttention"
         q, k, v = F.linear(x, self.qkvo_w[:3].flatten(end_dim=1)).view(B, T, 3 * self.num_heads, self.head_dim).chunk(3, dim=-2)
+        carry = prev_input.to(device=x.device, dtype=v.dtype).view_as(q)
         q, k = norm(q), norm(k) # QK norm @Grad62304977
+        q = q + carry
         q, k = self.rotary(q), self.rotary(k)
         v = norm(v)
-        prev_input = prev_input.view(B, T, self.num_heads, self.head_dim)
-        v = v + prev_input
         if ve is not None:
             v = self.lambdas[0] * v + self.lambdas[1] * ve.view_as(v) # @KoszarskyB & @Grad62304977
         else: # skip mid-layers token value embeddings by @YouJiacheng
