78890699777ce2748857a8fbbce7cf370855ec3e
Jaden Johnson <jadenjohnson@meta.com>
2026-01-08 23:51:19 -0500

    set fixed seed

--- train_gpt.py
+++ train_gpt.py
@@ -28,7 +28,18 @@ from torch import Tensor, nn
 
 dynamo.config.recompile_limit = 64
 
-
+def set_seed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    print(f"Random seed set as {seed}")
+set_seed()
 # -----------------------------------------------------------------------------
 # Custom operators: FP8 matmul by @YouJiacheng
 
