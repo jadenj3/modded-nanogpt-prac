d3b2f7626f3216e98be84de0a57c432f6ad8e212
Jaden Johnson <jadenjohnson@meta.com>
2026-02-06 21:51:43 -0500

    start adding token embeds

--- token_embeds.py
+++ token_embeds.py
@@ -6,5 +6,5 @@ vocab_size = 50304  # padded
 table = torch.zeros(vocab_size, max_bytes, dtype=torch.int32)
 for i in range(enc.n_vocab):
     b = list(enc.decode_single_token_raw(i))[:max_bytes]
-    table[i, :len(b)] = torch.tensor(b, dtype=torch.int32)
+    table[i, :len(b)] = torch.tensor(b, dtype=torch.int32) + 1
 torch.save(table, "data/spelling_table.pt")
