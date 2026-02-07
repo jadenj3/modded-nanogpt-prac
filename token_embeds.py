f590eaa71e90d49c517b14efc32428f013da2adb
Jaden Johnson <jadenjohnson@meta.com>
2026-02-06 22:37:00 -0500

    Fix token embeds

--- token_embeds.py
+++ token_embeds.py
@@ -5,7 +5,7 @@ max_bytes = 16
 vocab_size = 50304  # padded
 table = torch.zeros(vocab_size, max_bytes, dtype=torch.int32)
 for i in range(enc.n_vocab):
-    b = list(enc.decode_single_token_raw(i))[:max_bytes]
+    b = list(enc.decode_single_token_bytes(i))[:max_bytes]
     table[i, :len(b)] = torch.tensor(b, dtype=torch.int32) + 1
 
 # Sanity check
