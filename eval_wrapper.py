4d60d57e930c1b1266b8594812db5af1ca83e4b9
Jaden Johnson <jadenjohnson@meta.com>
2026-02-10 13:01:31 -0500

    Try to fix evals

--- eval_wrapper.py
+++ eval_wrapper.py
@@ -14,14 +14,12 @@ import os
 os.environ.setdefault("WORLD_SIZE", "1")
 os.environ.setdefault("RANK", "0")
 os.environ.setdefault("LOCAL_RANK", "0")
-os.environ.setdefault("MASTER_ADDR", "127.0.0.1")
-os.environ.setdefault("MASTER_PORT", "29500")
 
 import torch
 from dataclasses import dataclass
 
 # Import from train_gpt (now safe since training code is in __main__ block)
-from train_gpt import GPT, ForwardScheduleConfig, get_bigram_hash, args, ParamConfig
+from train_gpt import GPT, ForwardScheduleConfig, get_bigram_hash, ParamConfig
 
 # Make ParamConfig available in __main__ for torch.load unpickling
 import sys
