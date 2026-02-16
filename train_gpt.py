4157aa2a06f9691d95073f90a6452862513ea7aa
Jaden Johnson <jadenjohnson@meta.com>
2026-02-15 20:58:34 -0500

    Back to medium real quick

--- train_gpt.py
+++ train_gpt.py
@@ -545,7 +545,6 @@ class NorMuon(torch.optim.Optimizer):
         params_list = list(params)
         module_group_order = ['attn_gate', 'value_embed_gate', 'attn', 'mlp']  # 16, 10, 16, 32
         group_sizes = [16, 10, 16, 16, 16]
-        # 72... its actually 74
         params_list.sort(key=lambda x: module_group_order.index(x.label))
         print0(len(params_list), console=True)
         idx = 0
@@ -786,7 +785,7 @@ class DistAdam(torch.optim.Optimizer):
                 )
 
     def copy_lm_to_embed(self):
-        # run at 2/3 of training
+        # run at 1/6 of training
         lm_head = self.param_groups[0]['params'][0]
         embed = self.param_groups[-1]['params'][0]
         lm_head_state = self.state[lm_head]
@@ -863,7 +862,7 @@ def norm(x: Tensor):
 class CastedLinear(nn.Linear):
     def __init__(self, in_features: int, out_features: int, use_fp8=False, x_s=1.0, w_s=1.0, grad_s=1.0):
         super().__init__(in_features, out_features, bias=False)
-        self.use_fp8 = False  # use_fp8
+        self.use_fp8 = False  # turn off fp8 for now -> requires tuning of scales which hasnt been done on medium track
         self.x_s = x_s
         self.w_s = w_s
         self.grad_s = grad_s
@@ -1046,16 +1045,12 @@ class MLP(nn.Module):
 class Block(nn.Module):
     def __init__(self, dim: int, head_dim: int, num_heads: int, layer_idx: int):
         super().__init__()
-        # skip attention of blocks.6 (the 7th layer) by @YouJiacheng
         self.attn = CausalSelfAttention(dim, head_dim, num_heads, layer_idx)
-        # skip MLP blocks for first MLP layer by @EmelyanenkoK
         self.mlp = MLP(dim)
 
     def forward(self, x: Tensor, attn_args: AttnArgs):
-        if self.attn is not None:
-            x = x + self.attn(norm(x), attn_args)
-        if self.mlp is not None:
-            x = x + self.mlp(norm(x))
+        x = x + self.attn(norm(x), attn_args)
+        x = x + self.mlp(norm(x))
         return x
 
 
@@ -1151,6 +1146,7 @@ class GPT(nn.Module):
         self.scalars.lr_mul = 5.0
         self.scalars.wd_mul = 0.0
 
+        # start training with tied embed/lm_head
         self.split_embed = False
 
     def forward(self, input_seq: Tensor, target_seq: Tensor, seqlens: Tensor, schedule_cfg: ForwardScheduleConfig):
@@ -1246,7 +1242,7 @@ class GPT(nn.Module):
         logits_for_loss = logits.float() if not self.training else logits
 
         n_predict = mtp_weights.size(0) if mtp_weights is not None else 1
-        if self.training and n_predict > 1:
+        if n_predict > 1:
             # Multi-token prediction: take loss of the weighted average of next n_predict tokens
             logits_flat = logits_for_loss.view(-1, logits_for_loss.size(-1))
             idx = F.pad(target_seq, (0, n_predict - 1)).unfold(0, n_predict, 1)  # [T, n_predict] of shifted targets
@@ -1255,10 +1251,8 @@ class GPT(nn.Module):
             for k in range(1, n_predict):  # zero out preds past end of sequence
                 cross_entropy[-k:, k] = 0
             loss = (cross_entropy * mtp_weights).sum()
-        elif self.training:
-            loss = F.cross_entropy(logits_for_loss.view(-1, logits_for_loss.size(-1)), target_seq, reduction="sum")
         else:
-            loss = F.cross_entropy(logits_for_loss.view(-1, logits_for_loss.size(-1)), target_seq, reduction="mean")
+            loss = F.cross_entropy(logits_for_loss.view(-1, logits_for_loss.size(-1)), target_seq, reduction="sum")
         return loss
 
 
