d246b942f79a268d1ca87f085e52163e1c6b82d0
Jaden Johnson <jadenjohnson@meta.com>
2026-02-19 21:10:18 -0500

    fineweb 100b

--- train_gpt.py
+++ train_gpt.py
@@ -1663,8 +1663,8 @@ class TrainingManager():
 @dataclass
 class Hyperparameters:
     # data
-    train_files: str = "data/fineweb10B/fineweb_train_*.bin"  # input .bin to train on
-    val_files: str = "data/fineweb10B/fineweb_val_*.bin"  # input .bin to eval validation loss on
+    train_files: str = "data/fineweb/fineweb100B/fineweb_train_*.bin"  # input .bin to train on
+    val_files: str = "data/fineweb/fineweb100B/fineweb_val_*.bin"  # input .bin to eval validation loss on
     val_tokens: int = 10485760  # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
     # batch sizes
     train_bs_schedule: tuple = (131072, 262144, 393216, 524288,
